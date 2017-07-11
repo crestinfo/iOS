9ddad8a27a4f4cf6aa4bd2f1c0e45c02f8e4596a
Rakshit Patel <godfather.php@gmail.com>
2017-07-11 18:43:14 +0530

    API Integration Example

---
+++ APIDemo/apiDemo2_11_7/countryCell.swift
@@ -0,0 +1,25 @@
+//
+//  countryCell.swift
+//  apiDemo2_11_7
+//
+//  Created by Manish Vagh on 11/07/17.
+//  Copyright © 2017 Crest-Infotech. All rights reserved.
+//
+
+import UIKit
+
+class countryCell: UITableViewCell {
+
+    @IBOutlet weak var lblCountryName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
