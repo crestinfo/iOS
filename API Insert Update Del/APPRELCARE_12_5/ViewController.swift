f2596057aabed6fba7a064bfcffffe02dd0db7cb
Rakshit Patel <godfather.php@gmail.com>
2017-07-20 12:06:48 +0530

    API Ins UPD Del

---
+++ API Insert Update Del/APPRELCARE_12_5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APPRELCARE_12_5
+//
+//  Created by Manish Vagh on 12/05/17.
+//  Copyright © 2017 Crest-Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
